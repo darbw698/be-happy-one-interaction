9445848d201103ef039eb180ee7e37b50b30505e
Glitch (hello-webpage) <none>
2020-04-21 17:40:30 +0000

    🎊🍝 Checkpoint ./script.js:13732262/750

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("hello world");
+// console.log("hello world");
 
 // console.log ( $('.firstdiv'))
 
@@ -9,10 +9,10 @@ console.log($(".first-div"));
 console.log($(".second-div"));
 
 $(".first-div").show();
-// $('.first-div').hide()
+// $('.first-div').hide();
 
 // $(".second-div").hide();
-$('.second-div').show()
+$('.second-div').show();
 
 $(".first-div").html(
   "I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy "
@@ -22,6 +22,13 @@ $(".second-div").html(
   "I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy"
 );
 
-$('.second-div').on('click',(eventData)=>{
-  
+$('.first-div').on('click',(eventData)=>{
+  $(".first-div").hide();
+  $(".second-div").show();
 })
+
+$('.second-div').on('click',(eventData)=>{
+  $(".second-div").hide();
+  $(".first-div").show();
+})
+

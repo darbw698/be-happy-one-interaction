0fda50e692e6c76b1f0eb3ac7236d75c30a7092c
Glitch (hello-webpage) <none>
2020-04-21 17:17:30 +0000

    🍙🍚 Checkpoint ./script.js:13732262/1307 ./style.css:13732262/218

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-console.log ('hello world')
+console.log("hello world");
 
 // console.log ( $('.firstdiv'))
 
 // console.log ( $('.seconddiv'))
 
-console.log ($(".first-div"))
+console.log($(".first-div"));
 
-console.log ($(".second-div"))
+console.log($(".second-div"));
 
-$('.first-div').show()
+$(".first-div").show();
 // $('.first-div').hide()
 
-$('.second-div').hide()
+$(".second-div").hide();
 // $('.second-div').show()
 
-$('.first-div').css('')
+$(".first-div").html(
+  "I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy "
+);

7195a8d9b40c486e12d0076cd095ff6ff951e945
Glitch (hello-webpage) <none>
2020-04-22 03:37:49 +0000

    🍩🏊🏼 Checkpoint ./script.js:13732262/1204

--- script.js
+++ script.js
@@ -18,27 +18,53 @@ console.log($(".second-div"));
 
 
 $(".first-div").show();
-$(".second-div").show();
-$("").show();
-$("").show();
-$("").show();
-$("").show();
-$("").show();
-$("").show();
-$("").show();
-$("").show();
-$("").show();
-$("").show();
+$(".second-div").hide();
+$(".third-div").hide();
+$(".fourth-div").hide();
+$(".fifth-div").hide();
+$(".sixth-div").hide();
+$(".seventh-div").hide();
+$(".eighth-div").hide();
+$(".ninth-div").hide();
+$(".tenth-div").hide();
+$(".eleventh-div ").hide();
+$(".twelfth-div").hide();
 
 $(".first2-div").hide();
+$(".second2-div").hide();
+$(".third2-div").hide();
+$(".fourth2-div").hide();
+$(".fifth2-div").hide();
+$(".sixth2-div").hide();
+$(".seventh2-div").hide();
+$(".eighth2-div").hide();
+$(".ninth2-div").hide();
+$(".tenth2-div").hide();
+$(".eleventh2-div ").hide();
+$(".twelfth2-div").hide();
 
 
 $('.first-div').on('click',(eventData)=>{
   $(".first-div").hide();
-  $(".second-div").show();
+  $(".fifth2-div").show();
 })
 
-$('.second-div').on('click',(eventData)=>{
-  $(".second-div").hide();
-  $(".first-div").show();
+$(".fifth2-div").on('click',(eventData)=>{
+  $(".fifth2-div").hide();
+  $(".ninth-div").show();
+})
+
+$(".ninth-div").on('click',(eventData)=>{
+  $(".ninth-div").hide();
+  $(".third2-div").show();
+})
+
+$(".third2-div").on('click',(eventData)=>{
+  $(".third2-div").hide();
+  $(".twelfth-div").show();
+})
+
+$(".twelfth-div").on('click',(eventData)=>{
+  $(".twelfth-div").hide();
+  $(".first2-div").show();
 })

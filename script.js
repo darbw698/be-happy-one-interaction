8ad0de464309fd02779c24989d934e1b5fe7f66d
Glitch (hello-webpage) <none>
2020-04-22 02:50:50 +0000

    🗃🎉 Checkpoint ./style.css:13732262/4 ./animation.html:13732262/162 ./script.js:13732262/3846

--- script.js
+++ script.js
@@ -8,19 +8,12 @@ console.log($(".first-div"));
 
 console.log($(".second-div"));
 
+
 $(".first-div").show();
 // $('.first-div').hide();
 
 // $(".second-div").hide();
 $('.second-div').show();
-
-$(".first-div").html(
-  "I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy "
-);
-
-$(".second-div").html(
-  "I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy"
-);
 
 $('.first-div').on('click',(eventData)=>{
   $(".first-div").hide();
@@ -32,3 +25,10 @@ $('.second-div').on('click',(eventData)=>{
   $(".first-div").show();
 })
 
+// $(".first-div").html(
+//   "I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy I will be happy "
+// );
+
+// $(".second-div").html(
+//   "I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy I am happy"
+// );
